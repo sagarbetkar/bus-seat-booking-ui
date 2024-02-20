fdea91ecbea6230741b5377b1eca7882d1858778
Sagar Betkar <sagarbetkar1994@gmail.com>
2024-02-20 08:01:13 +0530

    Added dropdown

--- src/Home/Header/Header.js
+++ src/Home/Header/Header.js
@@ -1,13 +1,22 @@
 import { PATH } from '../../config/path.config.js'
-import './Header.css';
+import './Header.css'
 
 function Header(props) {
-
   return (
-      <header className="topnav">
-        <button onClick={() => props.setLink(PATH.DASH)}>Dashboard</button>
-        <button onClick={() => props.setLink(PATH.RESV)}>Reservation</button>
-      </header>
+    <header className='topnav'>
+      <button onClick={() => props.setLink(PATH.DASH)}>Dashboard</button>
+      <button onClick={() => props.setLink(PATH.RESV)}>Reservation</button>
+      <div class='dropdown'>
+        <button class='dropbtn'>
+          Dropdown
+          <i class='fa fa-caret-down'></i>
+        </button>
+        <div class='dropdown-content'>
+          <button onClick={() => props.setLink(PATH.DASH)}>Dashboard</button>
+          <button onClick={() => props.setLink(PATH.RESV)}>Reservation</button>
+        </div>
+      </div>
+    </header>
   )
 }
 
